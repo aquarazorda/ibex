b3e8cc7223a32e09cf87b594631e9f976532b688
Givi Gelashvili <givi.gelashvili@adjarabet.com>
2022-01-12 13:57:28 +0400

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Login } from './components/login/Login';
 import { Filter } from './components/filter/Filter';
+import { Routes, Route } from "react-router-dom";
 
 import './App.css';
 
@@ -8,8 +9,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
-        <Filter />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="filter" element={<Filter />} />
+        </Routes>
       </header>
     </div>
   );
